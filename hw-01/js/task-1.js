4f8d1fbf72ba7cc217a3178022aed4712b4363d1
Кирилл Гребень <atrakir@gmail.com>
2020-07-04 15:17:14 +0300

    task1/2/3 added

---
+++ hw-01/js/task-1.js
@@ -0,0 +1,10 @@
+console.log("Task 1")
+
+const name = "Генератор защитного поля";
+let price = 1000;
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
+price = 2000;
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
---
+++ hw-01/js/task-3.js
@@ -0,0 +1,17 @@
+console.log("Task 3")
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+
+const enterPassword = prompt("Введите пароль");
+
+if (enterPassword === null) {
+    message = 'Отменено пользователем!';
+}
+else if (enterPassword === 'jqueryismyjam') {
+    message = 'Добро пожаловать!';
+}
+else {
+    message = 'Доступ запрещен, неверный пароль!';
+}
+alert(message);
