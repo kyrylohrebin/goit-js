4ff5c772777a3183c12fad17c8e6cb6d7a4c0124
Кирилл Гребень <atrakir@gmail.com>
2020-07-12 13:22:03 +0300

    task 2 added

--- hw-02/js/task-02.js
+++ hw-02/js/task-02.js
@@ -1,3 +1,29 @@
 "use strict";
 
 console.log("Task 2");
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+  return message.split(" ").length * pricePerWord;
+};
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+);
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+);
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+);
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+);
