159297049b94f4ff4b9b249937043d5d2f915a17
Кирилл Гребень <atrakir@gmail.com>
2020-07-12 17:25:12 +0300

    task 6 added

---
+++ hw-02/js/task-06.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log("Task 6");
+
+let input;
+const numbers = [];
+let total = 0;
+
+do {
+  input = prompt("Введите число");
+
+  if (input !== null) {
+    if (Number.isNaN(+input)) {
+      alert("Это не число!");
+      continue;
+    }
+  }
+numbers.push(+input);
+
+} while (input !== null);
+
+for(const number of numbers){
+  total += number;
+}
+
+alert(`Общая сумма ${total}`);
