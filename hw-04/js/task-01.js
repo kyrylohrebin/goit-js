3b32d1b0daac2bd2cfa6925181c6916826dc4d7b
Кирилл Гребень <atrakir@gmail.com>
2020-07-25 11:31:19 +0300

    task-01 added

--- hw-04/js/task-01.js
+++ hw-04/js/task-01.js
@@ -8,22 +8,22 @@ const account = {
     discount: 0.1,
     orders: ['order-1', 'order-2', 'order-3'],
     changeDiscount(value) {
-      discount = value;
+      this.discount = value;
     },
     showOrders() {
       return this.orders;
     },
     addOrder(cost, order) {
-      balance -= cost;
-      orders.push(order);
+      this.balance -= cost;
+      this.orders.push(order);
     },
   };
   
   account.changeDiscount(0.15);
-  console.log(this.discount); // 0.15
+  console.log(account.discount); // 0.15
   
-  console.table(this.showOrders()); // ['order-1', 'order-2', 'order-3']
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
   
   account.addOrder(5000, 'order-4');
-  console.log(this.balance); // 19000
-  console.table(this.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+  console.log(account.balance); // 19000
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
