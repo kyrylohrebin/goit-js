2c256d1dbdf4f1c41c10c0761c32250567963bdd
Кирилл Гребень <atrakir@gmail.com>
2020-08-01 13:44:49 +0300

    task-01 added

---
+++ hw-05/js/task-01.js
@@ -0,0 +1,28 @@
+"use strict";
+
+console.log("Task 1");
+
+const Account = function({ login = "login", email = "email@mail.com" }) {
+    this.login = login;
+    this.email = email;
+};
+
+Account.prototype.getInfo = function() {
+    console.log(`login: ${this.login}, email: ${this.email}`);
+};
+
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
