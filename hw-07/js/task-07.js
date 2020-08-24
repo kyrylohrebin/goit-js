4f41a91390c5e21aa051ca86a798df9056e3479f
Кирилл Гребень <atrakir@gmail.com>
2020-08-24 15:03:06 +0300

    7 task fixed

--- hw-07/js/task-07.js
+++ hw-07/js/task-07.js
@@ -3,12 +3,8 @@ console.log("Task 7");
 const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
 
-inputRef.min = 0;
-inputRef.max = 50;
-spanRef.setAttribute('style', `font-size: ${inputRef.value}px`);
-
 const inputFontSize = event => {
-  event.target.spanRef.setAttribute('style', `font-size: ${event.target.spanRef.value}px`);
+  spanRef.style.fontSize = `${event.target.value}px`
 };
 
-inputFontSizeRef.addEventListener('change', inputFontSize);
+inputRef.addEventListener('change', inputFontSize);
