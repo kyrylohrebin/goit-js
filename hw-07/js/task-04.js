dba10f01e471d2b88169360c5ff8b7cfba54ef65
Кирилл Гребень <atrakir@gmail.com>
2020-08-25 20:08:17 +0300

    4 task fixed

--- hw-07/js/task-04.js
+++ hw-07/js/task-04.js
@@ -1,16 +1,16 @@
 console.log("Task 4");
 
-let counterValue = 0;
-
 const valueRef = document.querySelector("#value");
 const buttonDecrementRef = document.querySelector("#counter button[data-action='decrement']");
 const buttonIncrementRef = document.querySelector("#counter button[data-action='increment']");
 
 const increment = () => {
-    valueRef.textContent = counterValue++;
+    const counterValue = Number(valueRef.textContent) + 1;
+    valueRef.textContent = counterValue;
 }
 const decrement = () => {
-    valueRef.textContent = counterValue--;
+    const counterValue = Number(valueRef.textContent) - 1;
+    valueRef.textContent = counterValue;
 }
 
 buttonIncrementRef.addEventListener("click", increment);
