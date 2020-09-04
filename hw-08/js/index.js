685f9a94e4ca61c8986a6732a336c03c51eee311
Кирилл Гребень <atrakir@gmail.com>
2020-09-04 17:47:14 +0300

    arrows navigation added

--- hw-08/js/index.js
+++ hw-08/js/index.js
@@ -8,43 +8,76 @@ const galleryRef = document.querySelector(".js-gallery");
 Добавляем коллекцию
 */
 
-const createGalleryItems = (imagesArr) => {
-    return imagesArr.map((item) => {
-      const { preview, original, description } = item;
-      const li = document.createElement("li");
-      li.classList.add("gallery__item");
+function createElement(name, attrs = {}) {
+  const element = document.createElement(name);
+  for (let key in attrs) {
+    element.setAttribute(key, attrs[key]);
+  }
+  return element;
+}
 
-      const link = document.createElement("a");
-      link.classList.add("gallery__link");
-      link.setAttribute("href", preview);
-        
-      const img = document.createElement("img");
-      img.classList.add("gallery__image")
-      img.setAttribute("src", preview);
-      img.dataset.source = original;
-      img.setAttribute("alt", description);
-    
-      link.appendChild(img)
-      li.appendChild(link);
+function createGalleryItem(item) {
+  const galleryItem = createElement("li");
+  galleryItem.classList.add("gallery__item");
+  const galleryLink = createElement("a", {
+    href: item.original,
+  });
+  galleryLink.classList.add("gallery__link");
+  const galleryImage = createElement("img", {
+    src: item.preview,
+    "data-source": item.original,
+    alt: item.description,
+  });
+  galleryImage.classList.add("gallery__image");
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+  return galleryItem;
+}
 
-      return li;
-    });
-  };
+function createGallery() {
+  const gallery = galleryItems.map(createGalleryItem);
+  galleryRef.append(...gallery);
+}
 
-const imagesGallery = createGalleryItems(galleryItems);
-galleryRef.append(...imagesGallery);
+createGallery();
+
+/* const createGalleryItems = (imagesArr) => {
+  return imagesArr.map((item) => {
+    const { preview, original, description } = item;
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.setAttribute("href", preview);
+
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.setAttribute("src", preview);
+    img.dataset.source = original;
+    img.setAttribute("alt", description);
+
+    link.appendChild(img);
+    li.appendChild(link);
+
+    return li;
+  });
+}; */
+
+/* const imagesGallery = createGalleryItems(galleryItems);
+galleryRef.append(...imagesGallery); */
 
 /*
 Делегируем клик на картинку / открываем большую картинку
 */
 
-galleryRef.addEventListener('click', onImgClick);
-const lightboxImageRef = document.querySelector('.lightbox__image');
+galleryRef.addEventListener("click", onImgClick);
+const lightboxImageRef = document.querySelector(".lightbox__image");
 
-function onImgClick (event) {
+function onImgClick(event) {
   event.preventDefault();
 
-  if (event.target.nodeName !== 'IMG') {
+  if (event.target.nodeName !== "IMG") {
     return;
   }
 
@@ -66,26 +99,27 @@ function setLargeImageAlt(alt) {
 Модальное окно
 */
 
-const lightboxRef = document.querySelector('.js-lightbox');
-const lightboxBtnRef = document.querySelector('.lightbox__button');
-const lightboxContentRef = document.querySelector('.lightbox__content');
+const lightboxRef = document.querySelector(".js-lightbox");
+const lightboxBtnRef = document.querySelector(".lightbox__button");
+const lightboxContentRef = document.querySelector(".lightbox__content");
 
-
-lightboxBtnRef.addEventListener('click', closeOnBtn);
-lightboxContentRef.addEventListener('click', closeOnContent);
+lightboxBtnRef.addEventListener("click", closeOnBtn);
+lightboxContentRef.addEventListener("click", closeOnContent);
 
 function openModal() {
-  lightboxRef.classList.add('is-open');
+  lightboxRef.classList.add("is-open");
 
-  window.addEventListener('keydown', event => {
-    if (event.code === 'Escape') {
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
       closeModal();
     }
-  })
+  });
+
+  window.addEventListener("keydown", navigateArrows);
 }
 
 function closeModal() {
-  lightboxRef.classList.remove('is-open');
+  lightboxRef.classList.remove("is-open");
   deleteLargeImageSrc();
 }
 
@@ -103,3 +137,28 @@ function closeOnContent(event) {
   }
 }
 
+/*
+Стрелки
+*/
+
+function prevImage() {
+  const currentSrc = lightboxImageRef.getAttribute("src");
+  let currentIndex = galleryItems.findIndex((el) => el.original === currentSrc);
+  const prevIndex = galleryItems.find((el, i) => i === currentIndex - 1);
+  setLargeImageSrc(prevIndex.original);
+}
+
+function nextImage() {
+  const currentSrc = lightboxImageRef.getAttribute("src");
+  let currentIndex = galleryItems.findIndex((el) => el.original === currentSrc);
+  const nextIndex = galleryItems.find((el, i) => i === currentIndex + 1);
+  setLargeImageSrc(nextIndex.original);
+}
+
+function navigateArrows(event) {
+  if (event.code === "ArrowLeft") {
+    prevImage();
+  } else if (event.code === "ArrowRight") {
+    nextImage();
+  }
+}
